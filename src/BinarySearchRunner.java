06c69c1a7e216c74cf5dd5b33b2afb55b1659c49
Qihong Wu <qihongw3@nycstudents.net>
2022-03-03 20:35:35 -0500

    2nd commit

--- src/BinarySearchRunner.java
+++ src/BinarySearchRunner.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BinarySearchRunner
 {
   public static void main(String[] args)
@@ -45,6 +47,20 @@ public class BinarySearchRunner
     // alongside every search; this prints out what is being checked and what is happening
     
     int[] arr1 = {-10, -6, 0, 2, 12, 14, 17, 23, 35, 45, 49, 55, 81, 100, 108};
+    int[] arrayRandom = {1, 2, 3, 4, 5, 6, 7, 8,  234, 1000};
+    System.out.println(Arrays.binarySearch(arrayRandom, 234));
+    System.out.println(Arrays.binarySearch(arrayRandom, 5));
+    int[] arrayRandom2 = {-100, -56, 2, 6, 10, 29, 500};
+    System.out.println(Arrays.binarySearch(arrayRandom2, 2));
+    System.out.println(Arrays.binarySearch(arrayRandom2, 29));
+    System.out.println(Arrays.binarySearch(arrayRandom2, 0));
+
+    double[] arr2 = {-10.4, -9.0, 0.0, 0.22, 1.2, 1.4, 1.7, 2.3, 3.5, 4.5};
+    System.out.println(Arrays.binarySearch(arr2, 2.0));
+    System.out.println(Arrays.binarySearch(arr2, -10.0));
+    System.out.println(Arrays.binarySearch(arr2,0, 6,4.5));
+    System.out.println(Arrays.binarySearch(arr2,0, 6,0.22));
+
 /*
     // test when the target is in the middle (found on the first try!)
     int index = BinarySearch.binarySearchPrintedCommentary(arr1, 23);
